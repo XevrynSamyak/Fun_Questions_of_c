154e5d2cf67c703df8d1845341cc3a08492f7c8e
Samyak Jain <samjain0987654@gmail.com>
2026-09-17 03:25:39 +0530

    cd "/Users/samyakjain/Desktop/programming_c"

---
+++ trial_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int age = 18;
+    float cgpa = 8.7;
+    char section = 'A';
+
+    printf("Age = %d\n", age);
+    printf("CGPA = %.1f\n", cgpa);
+    printf("Section = %c\n", section);
+
+    return 0;
+}
